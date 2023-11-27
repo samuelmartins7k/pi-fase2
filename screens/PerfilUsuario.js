7b6e8d3d9e97e34a2b2337b2806b3c7ad6b4d734
Samuel Martins <samuel.am709@gmail.com>
2023-11-27 00:59:55 -0300

    atendendo issue #3

--- screens/PerfilUsuario.js
+++ screens/PerfilUsuario.js
@@ -1,30 +1,47 @@
 import React from "react";
-import { StyleSheet, Image, Text, View, Button, Alert } from "react-native";
-import { useAuth } from '../contexts/AuthContext'; 
+import { StyleSheet, Image, Text, View, TouchableOpacity, Alert } from "react-native";
+import { useAuth } from '../contexts/AuthContext';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function PerfilUsuario() {
   const { user, logout } = useAuth();
+  const navigation = useNavigation();
 
   const handleEditarDados = () => {
-    // Adicione aqui a lógica para editar dados do usuário
-    Alert.alert("Editar Dados", "Pressionou o botão 'Editar Dados'");
+    // Navegar para a tela Notes ao clicar em "Editar Dados"
+    navigation.navigate('Notes');
   };
 
   const handleSairDaConta = () => {
-    // Adicione aqui a lógica para sair da conta
-    logout(); // Chamando a função de logout do contexto
+    Alert.alert(
+      'Sair da Conta',
+      'Tem certeza que deseja sair da conta?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Sair',
+          onPress: async () => {
+            try {
+              await logout();
+              // Navegar para a tela Login ao fazer logout
+              navigation.navigate('Login');
+            } catch (error) {
+              console.error('Erro ao fazer logout:', error.message);
+            }
+          },
+        },
+      ],
+      { cancelable: false }
+    );
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.top}>
-        <Image
-          style={styles.vector}
-          source={{
-            uri:
-              "https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/srymul6nq7s-70%3A99?alt=media&token=1a8b176b-468e-4a4e-95d7-b30d28b57f06",
-          }}
-        />
         <Text style={styles.titulo}>Perfil de Usuário</Text>
       </View>
 
@@ -45,11 +62,21 @@ export default function PerfilUsuario() {
       </View>
 
       <View style={styles.opcoes}>
-        <View style={styles.divisor} />
-        <Button title="Editar Dados" onPress={handleEditarDados} />
-        <View style={styles.divisor} />
-        <Button title="Sair da conta" onPress={handleSairDaConta} />
-        <View style={styles.divisor} />
+        <TouchableOpacity
+          style={[styles.botao, { backgroundColor: "#27ae60" }]}
+          onPress={handleEditarDados}
+        >
+          <Icon name="home" size={20} color="#fff" style={styles.botaoIcone} />
+          <Text style={styles.botaoTexto}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.botao, { backgroundColor: "#e74c3c" }]}
+          onPress={handleSairDaConta}
+        >
+          <Icon name="sign-out" size={20} color="#fff" style={styles.botaoIcone} />
+          <Text style={styles.botaoTexto}>Sair</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -60,21 +87,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: "rgba(255,255,255,1)",
+    backgroundColor: "#F5F5F5",
     padding: 16,
   },
   top: {
     flexDirection: "row",
     alignItems: "center",
-  },
-  vector: {
-    width: 10.09,
-    height: 15.63,
+    marginBottom: 20,
   },
   titulo: {
     fontSize: 24,
     fontWeight: "bold",
-    marginLeft: 16,
+    marginLeft: 4,
+    alignItems: "center"
   },
   informacoes: {
     width: "100%",
@@ -86,20 +111,33 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 18,
     fontWeight: "bold",
-    marginBottom: 8, // Adicionando margem inferior entre o rótulo e o valor
+    marginBottom: 8,
   },
   valor: {
     fontSize: 18,
+    color: "#333",
   },
   opcoes: {
     flexDirection: "row",
     alignItems: "center",
-    marginTop: 16,
+    marginTop: 20,
   },
-  divisor: {
-    width: 5,
-    height: 15,
-    backgroundColor: "rgba(0, 0, 0, 1)",
+  botao: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
     marginHorizontal: 5,
+    borderRadius: 5,
+  },
+  botaoTexto: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 8,
+  },
+  botaoIcone: {
+    marginRight: 8,
   },
 });
